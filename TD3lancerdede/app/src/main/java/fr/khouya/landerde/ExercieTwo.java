d7cf997bbc054851662448cf7a6d0b13cca62847
Ayoub KHOUYA <a.khouya.ing@gmail.com>
2020-11-10 21:22:03 +0100

    Creation de l'activity lancer dé une seule fois

---
+++ TD3lancerdede/app/src/main/java/fr/khouya/landerde/ExercieTwo.java
@@ -0,0 +1,14 @@
+package fr.khouya.landerde;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExercieTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercie_two);
+    }
+}
